35f46b40c88b84b2d6d622ca14c89b47142dd4b2
gurman-man <coffeemangurman@gmail.com>
2026-01-02 23:44:16 +0200

    Day 16: Project 1 (WeSplit) - Overview and basic SwiftUI components

---
+++ Projects/01-WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by mac on 02.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let students = ["Harry", "Hermione", "Ron"]
+    @State private var selectedStudent = "Harry"
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+            .navigationTitle("Select a Student")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
