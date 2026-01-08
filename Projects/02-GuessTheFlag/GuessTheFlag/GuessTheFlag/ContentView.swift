cc2f556291dde6c773b1a40a63b15f12cf07ab09
gurman-man <coffeemangurman@gmail.com>
2026-01-08 12:16:14 +0200

    Day 20: Project 2 (GuessTheFlag) - Overview and using stacks to arrange views

---
+++ Projects/02-GuessTheFlag/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by mac on 08.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack() {
+            HStack() {
+                Text("x")
+                Text("x")
+                Text("x")
+            }
+            
+            HStack() {
+                Text("x")
+                Text("x")
+                Text("x")
+            }
+            
+            HStack() {
+                Text("x")
+                Text("x")
+                Text("x")
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/02-GuessTheFlag/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by mac on 08.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
