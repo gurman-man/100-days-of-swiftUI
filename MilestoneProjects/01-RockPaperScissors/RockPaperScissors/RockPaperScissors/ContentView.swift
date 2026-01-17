341d20d65c8f4683c354032d524aa6547302057b
gurman-man <coffeemangurman@gmail.com>
2026-01-17 23:58:59 +0200

    Create folder: MilsetoneProjects

---
+++ MilestoneProjects/01-RockPaperScissors/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by mac on 17.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var possibleMoves = ["Rock", "Paper", "Scissors"]
+    
+    var body: some View {
+        VStack {
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MilestoneProjects/01-RockPaperScissors/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by mac on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
