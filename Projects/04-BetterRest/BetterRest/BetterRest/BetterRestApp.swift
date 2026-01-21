9ab2d82ce8ad50c97de43b8e16221f4bcd3fcf80
gurman-man <coffeemangurman@gmail.com>
2026-01-21 12:08:13 +0200

    Day 26: Project 4 (BetterRest) - Overview and entering numbers with Stepper

---
+++ Projects/04-BetterRest/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by mac on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
