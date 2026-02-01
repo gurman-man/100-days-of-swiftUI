f460803cdbd0c42c534f8ee6edf14d6bd1d53587
gurman-man <coffeemangurman@gmail.com>
2026-02-01 19:59:05 +0200

    Day 32: Project 6 (Animations) - Overview & creating implicit animations

---
+++ Projects/06-Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by mac on 01.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/06-Animations/Animations/Animations/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by mac on 01.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button("Tap me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
