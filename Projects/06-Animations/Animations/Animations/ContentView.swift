738985b072069ad1a59f9f42d84163708c012864
gurman-man <coffeemangurman@gmail.com>
2026-02-03 15:01:24 +0200

    Showing and hiding views with transitions

--- Projects/06-Animations/Animations/Animations/ContentView.swift
+++ Projects/06-Animations/Animations/Animations/ContentView.swift
@@ -8,36 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    // Розбиваємо рядок на масив окремих символів
-    let letters = Array("Hello SwiftUI")
-    
-    @State private var enabled = false // Стан для перемикання кольору (синій/червоний)
-    @State private var dragAmount = CGSize.zero // Стан для відстеження координат руху пальця
-    
+    @State private var isShowingRed = false
     
     var body: some View {
-        // Перебираємо кожен символ за його індексом (num)
-        HStack(spacing: 0) {
-            ForEach(0..<letters.count, id: \.self) { num in
-                Text(String(letters[num]))
-                    .padding(5)
-                    .font(.title)
-                    .background(enabled ? .blue : .red)
-                    .offset(dragAmount) // Зміщуємо кожну літеру на відстань перетягування пальцем
-                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    // .transition(.scale)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
             }
         }
-        .gesture(
-            DragGesture()
-            // Спрацьовує постійно, поки ви ведете пальцем по екрану
-                .onChanged { dragAmount = $0.translation }
-            
-            // Спрацьовує один раз, коли ви відриваєте палець
-                .onEnded { _ in
-                    dragAmount = .zero // Повертаємо літери в початкову точку
-                    enabled.toggle() // Змінюємо стан кольору на протилежний
-                }
-        )
     }
 }
 
