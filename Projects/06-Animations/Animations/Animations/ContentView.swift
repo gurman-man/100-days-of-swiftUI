b969601427392efc756242e4a60af4b0852db6fc
gurman-man <coffeemangurman@gmail.com>
2026-02-03 14:49:09 +0200

    Animating gestures

--- Projects/06-Animations/Animations/Animations/ContentView.swift
+++ Projects/06-Animations/Animations/Animations/ContentView.swift
@@ -8,19 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    // Розбиваємо рядок на масив окремих символів
+    let letters = Array("Hello SwiftUI")
+    
+    @State private var enabled = false // Стан для перемикання кольору (синій/червоний)
+    @State private var dragAmount = CGSize.zero // Стан для відстеження координат руху пальця
+    
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        // Перебираємо кожен символ за його індексом (num)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount) // Зміщуємо кожну літеру на відстань перетягування пальцем
+                    .animation(.linear.delay(Double(num) / 20), value: dragAmount)
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .foregroundStyle(.white)
-        .animation(nil, value: enabled)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.spring(duration: 1, bounce: 0.9), value: enabled)
-        
+        .gesture(
+            DragGesture()
+            // Спрацьовує постійно, поки ви ведете пальцем по екрану
+                .onChanged { dragAmount = $0.translation }
+            
+            // Спрацьовує один раз, коли ви відриваєте палець
+                .onEnded { _ in
+                    dragAmount = .zero // Повертаємо літери в початкову точку
+                    enabled.toggle() // Змінюємо стан кольору на протилежний
+                }
+        )
     }
 }
 
