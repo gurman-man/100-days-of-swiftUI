0fc996ded5d977b8159e16ce252ab87bf5ba0ae4
gurman-man <coffeemangurman@gmail.com>
2026-02-06 21:27:59 +0200

    Day 35: Milestone Project 2 (Edutainment) - Starting build app

---
+++ MilestoneProjects/02-Edutainment/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by mac on 06.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
