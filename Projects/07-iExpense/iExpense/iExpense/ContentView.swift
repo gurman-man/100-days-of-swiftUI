4f1667d00682c1a084c5f3af68951d817b9f38db
gurman-man <coffeemangurman@gmail.com>
2026-02-11 20:35:40 +0200

    Day 36: Project 7 (iExpense) - Overview & using @State with classes

---
+++ Projects/07-iExpense/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by mac on 11.02.2026.
+//
+
+import SwiftUI
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Bagginns"
+}
+
+struct ContentView: View {
+    @State private var user = User()
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Projects/07-iExpense/iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by mac on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
