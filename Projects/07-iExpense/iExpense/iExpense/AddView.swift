43e4663ab3465797c5859f0f71f371c2a8d83887
gurman-man <coffeemangurman@gmail.com>
2026-02-12 16:13:17 +0200

    Sharing an observed object with a new view

---
+++ Projects/07-iExpense/iExpense/iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by mac on 12.02.2026.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    // Локальні стани для збереження значень полів введення
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0
+    
+    // Посилання на існуючий екземпляр класу Expenses
+    var expenses: Expenses
+    
+    let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text("\($0)")
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD")).keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- Projects/07-iExpense/iExpense/iExpense/ContentView.swift
+++ Projects/07-iExpense/iExpense/iExpense/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// Модель даних для однієї витрати. Identifiable дозволяє ForEach працювати без id: \.self
 struct ExpenseItem: Identifiable {
     let id = UUID()
     let name: String
@@ -14,13 +15,17 @@ struct ExpenseItem: Identifiable {
     let amount: Double
 }
 
+// Клас для зберігання списку витрат
+// @Observable робить клас "розумним": SwiftUI автоматично оновить інтерфейс, коли список items зміниться
 @Observable
-class Expense {
+class Expenses {
     var items = [ExpenseItem]()
 }
 
 struct ContentView: View {
-    @State private var expenses = Expense()
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false // Стан для керування 2 екраном
     
     var body: some View {
         NavigationStack {
@@ -28,14 +33,17 @@ struct ContentView: View {
                 ForEach(expenses.items) {
                     Text("\($0.name)")
                 }
-                .onDelete(perform: removeItems)
+                .onDelete(perform: removeItems) // Додаємо можливість видалення свайпом
             }
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    showingAddExpense = true
                 }
+            }
+            // Викликаємо AddView та ПЕРЕДАЄМО йому наш об'єкт expenses
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
             }
         }
     }
