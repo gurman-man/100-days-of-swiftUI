e628434a1518d635fc5b172c0a46fd249d8d2395
gurman-man <coffeemangurman@gmail.com>
2026-02-18 09:37:35 +0200

    Day 41: Project 8 (Moonshot) - Implementing & Showing mission details with ScrollView and containerRelativeFrame()

--- Projects/08-Moonshot/Moonshot/Moonshot/Views/MissionView.swift
+++ Projects/08-Moonshot/Moonshot/Moonshot/Views/MissionView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct MissionView: View {
+    let mission: Mission
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                // Image
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                
+                // Content
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+                    
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
     }
 }
 
 #Preview {
-    MissionView()
+    // Передаємо об'єкт Mission, щоб вона мала що відображати
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
 }
