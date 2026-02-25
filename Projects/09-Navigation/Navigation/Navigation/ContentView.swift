eab1c4290afc057cd9b703661f19e3e8193a7f8e
gurman-man <coffeemangurman@gmail.com>
2026-02-25 12:50:29 +0200

    Day 45: Project 9 (Navigation) - Implementing & Customizing the navigation bar appearance

--- Projects/09-Navigation/Navigation/Navigation/ContentView.swift
+++ Projects/09-Navigation/Navigation/Navigation/ContentView.swift
@@ -7,63 +7,18 @@
 
 import SwiftUI
 
-@Observable
-class PathStore {
-    var path: NavigationPath {
-        didSet {
-            save()
-        }
-    }
-    
-    private let savePath = URL.documentsDirectory.appending(path: "Saved Path")
-    
-    // ініціалізатор для завантаження даних з диску та встановлення у шлях масиву
-    init() {
-        // спроба витягнути дані
-        if let data = try? Data(contentsOf: savePath) {
-            // спроба декодувати шлях
-            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
-                path = NavigationPath(decoded)
-                return
-            }
-        }
-        
-        // якщо не вдалось декодувати шлях чи витягнути дані
-        path = NavigationPath()
-    }
-    
-    func save() {
-        guard let representation = path.codable else { return }
-        
-        do {
-            let data = try JSONEncoder().encode(representation)
-            try data.write(to: savePath)
-        } catch {
-            print("Failed to save navigation data")
-        }
-    }
-}
-
-struct DetailView: View {
-    var number: Int
-    
-    var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore() // для збереження стану
-    
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i)
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbarBackground(.blue, for: .navigationBar)
+            .toolbarColorScheme(.dark)
+            .toolbar(.hidden, for: .navigationBar)
         }
-        .padding()
     }
 }
 
