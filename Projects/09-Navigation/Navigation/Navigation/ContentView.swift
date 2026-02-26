90a9b36c29c132e9c99db1a410a0198479288c87
gurman-man <coffeemangurman@gmail.com>
2026-02-27 00:15:23 +0200

    Day 46: Project 9 (Navigation) - Complete all challenges

--- Projects/09-Navigation/Navigation/Navigation/ContentView.swift
+++ Projects/09-Navigation/Navigation/Navigation/ContentView.swift
@@ -7,6 +7,18 @@
 
 import SwiftUI
 
+// MARK: - Challenges - Day46
+
+/*
+    1. Change project 7 (iExpense) so that it uses NavigationLink for adding new expenses rather than a sheet. (Tip: The dismiss() code works great here, but you might want to add the navigationBarBackButtonHidden() modifier so they have to explicitly choose Cancel.)
+ 
+    2. Try changing project 7 so that it lets users edit their issue name in the navigation title rather than a separate textfield. Which option do you prefer?
+ 
+    3. Return to project 8 (Moonshot), and upgrade it to use NavigationLink(value:). This means adding Hashable conformance, and thinking carefully how to use navigationDestination().
+*/
+
+// MARK: - Imlementation
+
 struct ContentView: View {
     @State private var title = "SwiftUI"
     
