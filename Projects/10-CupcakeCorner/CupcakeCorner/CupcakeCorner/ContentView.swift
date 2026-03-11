427aa6f5c3cb829cc31023d50ca891d5824766dc
gurman-man <coffeemangurman@gmail.com>
2026-03-11 16:33:41 +0200

    Validating and disabling forms

--- Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,26 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            
-            // 1. Перевіряємо, чи картинка успішно завантажена
-            if let image = phase.image {
-                image
-                    .resizable()    // Дозволяємо змінювати розмір
-                    .scaledToFit()  // Масштабуємо, щоб вписати в рамки без спотворен
-                
-            // 2. Якщо картинки немає, перевіряємо, чи виникла помилка
-            } else if let error = phase.error {
-                Text("Помилка: \(error.localizedDescription)")
-                
-            // 3. Якщо немає ні картинки, ні помилки — значить іде процес завантаження
-            } else {
-                ProgressView()
-            }
+    
+    @State private var username = ""
+    @State private var email = ""
+    
+    var disableForm: Bool {
+        // 1. Очищуємо дані від пробілів по боках
+        let cleanUsername = username.trimmingCharacters(in: .whitespacesAndNewlines)
+        let cleanEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        // 2. Перевіряємо довжину
+        if cleanEmail.count < 5 || cleanUsername.count < 5 {
+            return true
         }
-        // Встановлюємо жорсткі розміри для всього контейнера AsyncImage
-        .frame(width: 200, height: 200)
+        
+        // 3. Перевіряємо наявність символів @ та . в імейлі
+        if cleanEmail.contains("@") == false || cleanEmail.contains(".") == false {
+            return true
+        }
+        
+        // Якщо всі перевірки пройдено — розблоковуємо кнопку
+        return false
+        
+        
+        // КОРОТКА ВЕРСІЯ
+        /*
+            username.trimmingCharacters(in: .whitespacesAndNewlines).count < 5 ||
+            email.trimmingCharacters(in: .whitespacesAndNewlines).count < 5 ||
+            !email.contains("@") ||
+            !email.contains(".")
+        */
+    }
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("UserName", text: $username)
+                TextField("Email", text: $email)
+            }
+            
+            Section {
+                Button("Create account") {
+                    print("Created account...")
+                }
+            }
+            .disabled(disableForm)
+        }
     }
 }
 
