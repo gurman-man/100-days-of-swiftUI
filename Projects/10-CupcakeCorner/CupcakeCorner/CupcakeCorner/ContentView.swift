94fd797e439644f7e86c82d4a6c53a85b785cf9f
gurman-man <coffeemangurman@gmail.com>
2026-03-12 21:59:11 +0200

    Adding Codable conformance to an @Observable class

--- Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,53 +7,24 @@
 
 import SwiftUI
 
+@Observable
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _name = "name"
+    }
+    var name = "Taylor"
+}
+
 struct ContentView: View {
     
-    @State private var username = ""
-    @State private var email = ""
-    
-    var disableForm: Bool {
-        // 1. Очищуємо дані від пробілів по боках
-        let cleanUsername = username.trimmingCharacters(in: .whitespacesAndNewlines)
-        let cleanEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
-        
-        // 2. Перевіряємо довжину
-        if cleanEmail.count < 5 || cleanUsername.count < 5 {
-            return true
-        }
-        
-        // 3. Перевіряємо наявність символів @ та . в імейлі
-        if cleanEmail.contains("@") == false || cleanEmail.contains(".") == false {
-            return true
-        }
-        
-        // Якщо всі перевірки пройдено — розблоковуємо кнопку
-        return false
-        
-        
-        // КОРОТКА ВЕРСІЯ
-        /*
-            username.trimmingCharacters(in: .whitespacesAndNewlines).count < 5 ||
-            email.trimmingCharacters(in: .whitespacesAndNewlines).count < 5 ||
-            !email.contains("@") ||
-            !email.contains(".")
-        */
+    var body: some View {
+        Button("Encode Taylor", action: encodeTaylor)
     }
     
-    var body: some View {
-        Form {
-            Section {
-                TextField("UserName", text: $username)
-                TextField("Email", text: $email)
-            }
-            
-            Section {
-                Button("Create account") {
-                    print("Created account...")
-                }
-            }
-            .disabled(disableForm)
-        }
+    func encodeTaylor() {
+        let data = try! JSONEncoder().encode(User())
+        let str = String(decoding: data, as: UTF8.self)
+        print(str)
     }
 }
 
