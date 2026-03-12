9c9c9b56cc5769f101f24bd547b0d2bd0ee2be80
gurman-man <coffeemangurman@gmail.com>
2026-03-13 00:24:56 +0200

    Taking basic order details

--- Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -5,60 +5,53 @@
 //  Created by mac on 10.03.2026.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var engine: CHHapticEngine?
+    
+    @State private var order = Order() // щоб всіекрани працювали з тими самими даними
     
     var body: some View {
-        Button("Play Haptic", action: complexSuccess)
-            .onAppear(perform: prepareHaptics) // гарантує запуск системи тактильного відгуку
-        
-//            .sensoryFeedback(.increase, trigger: counter) - всбудований тактильний відгук Apple (increse - один із режимів)
-    }
-    
-    func prepareHaptics() {
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        
-        do {
-            engine = try CHHapticEngine()
-            try engine?.start()
-        } catch {
-            print("There was an error creating the engine: \(error.localizedDescription)")
+        NavigationStack {
+            VStack {
+                
+                Form {
+                    // section of Cupcake-Type & Cupcake-Quantity
+                    Section {
+                        Picker("Select your cacke type", selection: $order.type) {
+                            ForEach(Order.types.indices, id: \.self) {
+                                Text(Order.types[$0])
+                            }
+                        }
+                        
+                        Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                    }
+                    
+                    // section with extra supplements
+                    Section {
+                        Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                        
+                        if order.specialRequestEnabled {
+                            Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                            
+                            Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                        }
+                    }
+                    
+                    // section of Navigation to AdressView
+                    Section {
+                        NavigationLink("Delivery details") {
+                            AddressView(order: order)
+                        }
+                    }
+                    
+                }
+                .navigationTitle("Cupcake Corner")
+                
+            }
         }
     }
     
-    func complexSuccess() {
-        // make sure that the device supports haptics
-        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
-        var events = [CHHapticEvent]()
-        
-        // create one intense, sharp tap
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
-            events.append(event)
-        }
-        
-        for i in stride(from: 0, to: 1, by: 0.1) {
-            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
-            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
-            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
-            events.append(event)
-        }
-        
-        // convert those events into a pattern and play it immediately
-        do {
-            let pattern = try CHHapticPattern(events: events, parameters: [])
-            let player = try engine?.makePlayer(with: pattern)
-            try player?.start(atTime: 0)
-        } catch {
-            print("Failed to play pattern: \(error.localizedDescription)")
-        }
-    }
 }
 
 #Preview {
---
+++ Projects/10-CupcakeCorner/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by mac on 12.03.2026.
+//
+
+import SwiftUI
+
+@Observable
+class Order {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet { // дозволяє відслідковувати та одразу змінювати
+            extraFrosting = false
+            addSprinkles = false
+        }
+    }
+
+    var extraFrosting = false // глазур
+    var addSprinkles = false // посипка
+}
