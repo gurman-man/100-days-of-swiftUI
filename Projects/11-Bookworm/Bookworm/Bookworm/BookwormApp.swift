eafe20fa0d7928d191b8daa16ae499903719bc6c
gurman-man <coffeemangurman@gmail.com>
2026-03-17 21:29:54 +0200

    Day 53: Project 11 (Bookworm) - Overview & Creating a custom component with @Binding

---
+++ Projects/11-Bookworm/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by mac on 17.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
