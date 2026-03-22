709a38d49ef549247eb6ca219f2bd9ea720250ce
gurman-man <coffeemangurman@gmail.com>
2026-03-22 18:26:24 +0200

    Day 55: Project 11 (Bookworm) - Implementing 2 & Showing book details

---
+++ Projects/11-Bookworm/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,63 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by mac on 22.03.2026.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        // 1. Конфігурація: зберігаємо дані лише в оперативній пам'яті (RAM)
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        
+        // 2. Сховище: ініціалізуємо базу даних для моделі Book
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        
+        // 3. Mock Data: створюємо тестовий об'єкт для відображення
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it!", rating: 4)
+        
+        // 4. Ін'єкція: передаємо дані у View та підключаємо до контейнера
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed tp create preview:  \(error.localizedDescription)")
+    }
+}
