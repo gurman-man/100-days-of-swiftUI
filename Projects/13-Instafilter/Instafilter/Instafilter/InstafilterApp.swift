524cca52128e03f3d8be1788236a4d63de088e52
gurman-man <coffeemangurman@gmail.com>
2026-04-18 12:17:38 +0300

    Day 62: Project 13 (Instafilter) - Overview & How property wrappers become structs

---
+++ Projects/13-Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by mac on 18.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
