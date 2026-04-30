d61de49c049f768989aaf73d7cc69da79df80c33
gurman-man <coffeemangurman@gmail.com>
2026-04-30 16:42:41 +0300

    Day 64: Project 13 (Instafilter) - Overview & Loading photos from the user's photo library

--- Projects/13-Instafilter/Instafilter/Instafilter/ContentView.swift
+++ Projects/13-Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -5,26 +5,54 @@
 //  Created by mac on 18.04.2026.
 //
 
+import PhotosUI // Фреймворк, для роботи з системним вікном вибору фото
 import SwiftUI
 
 struct ContentView: View {
+    // Це не саме зображення, а масив "посилань" на вибрані фото
+    @State private var pickerItems = [PhotosPickerItem]()
+    
+    // Масив готових SwiftUI зображень для відображення на екрані
+    @State private var selectedImages = [Image]()
+    
     var body: some View {
-        
-        // Short version
-        ContentUnavailableView("No snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet"))
-        
-        Divider()
-        
-        // Full-control version
-        ContentUnavailableView {
-            Label("No snippets", systemImage: "swift")
-        } description: {
-            Text("You don't have any saved snippets yet")
-        } actions: {
-            Button("Create snnipet") {
-                // create a snippet
+        VStack {
+            // Конфігурація пікера
+            PhotosPicker(
+                selection: $pickerItems,
+                maxSelectionCount: 5, // Обмежуємо вибір до 5 елементів
+                matching: .any(of: [.images, .not(.screenshots)]) // Фільтр: фото, але не скріншоти
+            ) {
+                // Кастомізація зовнішнього вигляду кнопки
+                Label("Select a picture", systemImage: "photo")
             }
-            .buttonStyle(.borderedProminent)
+            
+            // Відображення вибраних зображень
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+            }
+        }
+        // Відстежуємо зміну pickerItems (коли користувач натиснув "Done")
+        .onChange(of: pickerItems) {
+            Task {
+                // Очищаємо попередні фото перед новим завантаженням
+                selectedImages.removeAll()
+                
+                // Перебираємо всі вибрані "посилання"
+                for item in pickerItems {
+                    // Асинхронно намагаємось завантажити дані як Image
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
         }
     }
 }
