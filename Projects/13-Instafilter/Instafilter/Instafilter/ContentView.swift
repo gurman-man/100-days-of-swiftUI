573a957bcb9ca7968520b4afe770fedb35254a3c
gurman-man <coffeemangurman@gmail.com>
2026-04-30 18:29:49 +0300

    How to ask the user to leave an App Store review

--- Projects/13-Instafilter/Instafilter/Instafilter/ContentView.swift
+++ Projects/13-Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -5,47 +5,30 @@
 //  Created by mac on 18.04.2026.
 //
 
+import StoreKit // Фреймворк для роботи з App Store (відгуки, покупки)
 import SwiftUI
 
 struct ContentView: View {
+    // Читаємо системний метод для виклику вікна оцінки
+    @Environment(\.requestReview) var requestReview
+    
+    // Зберігаємо кількість дій у пам'ять телефону (не зникає після перезапуску)
+    @AppStorage("processCount") var processCount = 0
+    
     var body: some View {
-        List {
-            // Базовий варіант (текст "Share" + іконка)
-            Section("Default Share") {
-                ShareLink(item: URL(string: "https://www.hackingwithswift.com")!)
-            }
-            
-            
-            // З додаванням теми (subject) та опису (message)
-            Section("Share with Metadata") {
-                ShareLink(item: URL(
-                    string: "https://www.hackingwithswift.com")!,
-                          subject: Text("Learn Swift here"),
-                          message: Text("Check out the 100 Days of SwiftUI")
-                )
-            }
-            
-            // З кастомним виглядом кнопки
-            Section("Custom Label") {
-                ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
-                    Label("Spread the word about Swift", systemImage: "swift")
-                }
-            }
-            
-            
-            // Поширення фото (потребує обов'язкове прев'ю)
-            Section("Media Sharing") {
-                let example = Image(.skating)
-                
-                ShareLink(
-                    item: example,
-                    preview: SharePreview("Skating photo", image: example)
-                ) {
-                    Label("Click to share", systemImage: "skateboard.fill")
-                }
+        VStack {
+            Button("Leave a review") {
+                // Викликаємо
+                reviewAction()
             }
         }
-        .listStyle(.insetGrouped)
+    }
+    
+    func reviewAction() {
+        processCount += 1
+        if processCount >= 5 {
+            requestReview() // Просимо відгук, коли юзер вже "втягнувся"
+        }
     }
 }
 
