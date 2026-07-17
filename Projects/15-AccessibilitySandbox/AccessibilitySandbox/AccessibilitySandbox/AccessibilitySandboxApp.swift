bc31e59680a84a86cbc2d704a0044ed0a699b8e2
Max Ivasechko <maksikkoval22@gmail.com>
2026-07-17 17:05:25 +0300

    Day 74: Project 15 (AccessibilitySandbox) - Overview 2 & Identifying views with useful labels

---
+++ Projects/15-AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by mac on 17.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Projects/15-AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by mac on 17.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
+    var body: some View {
+        VStack {
+            Button {
+                selectedPicture = Int.random(in: 0...3)
+            } label: {
+                Image(pictures[selectedPicture])
+                    .resizable()
+                    .scaledToFit()
+            }
+            .accessibilityLabel(labels[selectedPicture])
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
